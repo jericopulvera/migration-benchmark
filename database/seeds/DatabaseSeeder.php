ca08088ec5a667ac4cd2e869c49f53c9ce5aaa45
jericopulvera <jerico.pulvera01z@gmail.com>
2017-10-31 19:08:27 +0800

    debugging slow performance

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,10 +2,17 @@
 
 use Illuminate\Database\Seeder;
 
+use Faker\Generator as Faker;
 use App\User;
 
 class DatabaseSeeder extends Seeder
 {
+	protected $faker;
+
+	public function __construct(Faker $faker)
+	{
+		$this->faker = $faker;
+	}
     /**
      * Run the database seeds.
      *
@@ -13,8 +20,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-    	$start = microtime(true);
-    	factory(User::class, 10000)->create();
+    	DB::connection()->disableQueryLog();
+
+    	User::create([
+    		'name' => $this->faker->name,
+    		'email' => $this->faker->unique()->safeEmail,
+    		'password' => $password ?: $password = bcrypt('secret'),
+    		'remember_token' => str_random(10),
+    	]);
+
     	$secs = microtime(true) - $start;
     	echo "Finished in $secs \n"; 
     }
